9dbeef2c68c42d51435a925f655710b4e361681f
bendrendel <bfdrendel@gmail.com>
2021-04-06 22:16:01 -0600

    Change way screens fly in and out

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-const screens = document.querySelectorAll('.screen');
-const playBtn = document.getElementById('play-btn');
-const insectBtns = document.querySelectorAll('.insect-btn')
+const titleScreen = document.querySelector('.title-screen');
+const selectScreen = document.querySelector('.select-screen');
+const gameScreen = document.querySelector('.game-screen');
+const playBtn = document.querySelector('.play-btn');
+const insectBtns = document.querySelectorAll('.insect-btn');
 
 playBtn.addEventListener('click', () => {
-    screens.forEach(screen => {
-        screen.style.transform = 'translateY(-100vh)';
-    })
+    titleScreen.classList.remove('show');
+    titleScreen.classList.add('hide');
+    selectScreen.classList.add('show');
 });
 
 insectBtns.forEach(insectBtn => {
-    insectBtn.addEventListener('click', e => {
-        screens.forEach((screen) => {
-            screen.style.transform = 'translateY(-200vh)';
-        });
+    insectBtn.addEventListener('click', () => {
+        // selectScreen.classList.remove('show');
+        selectScreen.classList.add('hide');
+        gameScreen.classList.add('show');
     })
 });
