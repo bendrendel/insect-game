1c428cb02ff28e64dd7519cd8fe6d8393258e37f
bendrendel <bfdrendel@gmail.com>
2021-04-08 13:34:50 -0600

    Changes based on solution

--- script.js
+++ script.js
@@ -7,10 +7,7 @@ const insectBtns = document.querySelectorAll('.insect-btn');
 const scoreEl = document.getElementById('score');
 const timerEl = document.getElementById('timer');
 const message = document.querySelector('.message');
-let insectImageSrc;
-let insectImageAlt;
 let score = 0;
-let startTime;
 
 playBtn.addEventListener('click', () => {
     titleScreen.classList.add('hide');
@@ -22,33 +19,38 @@ insectBtns.forEach(insectBtn => {
         selectScreen.classList.remove('show');
         selectScreen.classList.add('hide');
         gameScreen.classList.add('show');
-        insectImageSrc = insectBtn.querySelector('img').src;
-        insectImageAlt = insectBtn.querySelector('img').alt;
+
+        const insectImageSrc = insectBtn.querySelector('img').src;
+        const insectImageAlt = insectBtn.querySelector('img').alt;
+
         setTimeout(() => {
-            addInsect();
+            addInsect(insectImageSrc, insectImageAlt);
             startTimer();
         }, 500);
     })
 });
 
-function addInsect() {
+function addInsect(insectImageSrc, insectImageAlt) {
     let insectImage = document.createElement('img');
-    insectImage.src = insectImageSrc;
-    insectImage.alt = insectImageAlt;
     insectImage.classList.add('insect-image');
     insectImage.classList.add('appear');
+    
+    insectImage.src = insectImageSrc;
+    insectImage.alt = insectImageAlt;
+
     insectImage.style.top = `${Math.random() * 90 + 5}%`;
     insectImage.style.left = `${Math.random() * 90 + 5}%`;
     insectImage.style.transform = `translate(-50%, -50%) rotate(${Math.random() * 360}deg)`;
 
     insectImage.addEventListener('click', () => {
+        incrementScore();
+        
         insectImage.classList.remove('appear');
         insectImage.classList.add('disappear');
+        
         setTimeout(() => insectImage.remove(), 200);
-        setTimeout(() => addInsect(), 500);
-        setTimeout(() => addInsect(), 700);
-
-        incrementScore();
+        setTimeout(() => addInsect(insectImageSrc, insectImageAlt), 500);
+        setTimeout(() => addInsect(insectImageSrc, insectImageAlt), 700);
     });
 
     playArea.append(insectImage);
@@ -56,6 +58,7 @@ function addInsect() {
 
 function incrementScore() {
     score++;
+
     scoreEl.innerHTML = '';
     scoreEl.innerHTML = `<p id="score">Score: ${score.toString()}</p>`;
 
@@ -65,12 +68,17 @@ function incrementScore() {
 }
 
 function startTimer() {
-    startTime = new Date();
+    const startTime = new Date();
+    
     setInterval(() => {
         const currentTime = new Date();
+    
         const elapsedMilliseconds = currentTime - startTime;
         const elapsedSeconds = Math.floor((elapsedMilliseconds / 1000) % 60);
         const elapsedMinutes = Math.floor((elapsedMilliseconds / 1000) / 60);
-        timerEl.innerHTML = `<p>Time: ${elapsedMinutes.toString().padStart(2,'0')}:${elapsedSeconds.toString().padStart(2,'0')}`;
+        
+        console.log(elapsedMilliseconds, elapsedSeconds, elapsedMinutes);
+        
+        timerEl.innerHTML = `<p>Time: ${elapsedMinutes.toString().padStart(2, '0')}:${elapsedSeconds.toString().padStart(2, '0')}`;
     }, 1000)
 }
